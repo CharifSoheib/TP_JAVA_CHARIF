604b451ec4cafa2e2fe830c2170e8abcf406ffbc
Charif Soheib Abdelkader <sohaib806@live.fr>
2023-10-12 22:44:45 +0100

    Test unitaires qui manquent apres le Pit test report

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -136,6 +136,53 @@ class GildedRoseTest {
     assertEquals("foo, 0, 0", item.toString());
   
   }
+
+
+  @Test
+  public void testQualityNeverNegative() {
+    app.items[3].sellIn = 0;
+    app.items[3].quality = 0;
+    app.updateQuality();
   
+    // Test pour normal item qualité jamais negative
+    assertEquals("Normal Item", app.items[3].name);
+    assertEquals(-1, app.items[3].sellIn);
+    assertEquals(0, app.items[3].quality);
+  }
+  
+  @Test
+  public void testBackstageQualityIncreaseSellInMoreThanEleven() {
+    app.items[1].sellIn = 11;
+    app.updateQuality();
+  
+    // Test pour Backstage passes qualité incremente sellIn > 11
+    assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[1].name);
+    assertEquals(10, app.items[1].sellIn);
+    assertEquals(31, app.items[1].quality);
+  }
+  @Test 
+  public void testBackstageQualityIncreaseSellInMoreThanOne() {
+    app.items[1].sellIn = 1;
+    app.updateQuality();
+  
+    // Test pour Backstage passes qualité incremente  quand sellIn > 1
+    assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[1].name);
+    assertEquals(0, app.items[1].sellIn);
+    assertEquals(33, app.items[1].quality);
+  
+  }
+
+  @Test
+public void testBackstageQualityIncreaseSellInMoreThanSix() {
+  app.items[1].sellIn = 6;
+  app.updateQuality();
+
+  // Test pour Backstage passes qualité incremente sellIn > 6
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[1].name);
+  assertEquals(5, app.items[1].sellIn);
+  assertEquals(32, app.items[1].quality);} 
 }
+
+  
+
 
